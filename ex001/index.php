ca55cbbdd9411b3d11c622e7d6c69ee4b504025d
Mireli Araujo <mirellypollocks@gmail.com>
2023-10-07 17:00:16 -0300

    Atualização exercicios 0 e 1, me livrando da maldição

--- ex001/index.php
+++ ex001/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dados do servidor</title>
 </head>
 <body>
-    
+    <h1>Dados do Servidor:</h1>
+    <?php 
+        phpinfo(); 
+    ?>
 </body>
 </html>
